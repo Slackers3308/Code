76f9e85e63c6b1bcb35a0a89f871d78ee4d29187
Aniket Bhatt <AniketMan@users.noreply.github.com>
2018-10-24 18:55:25 -0600

    Data Tables

---
+++ SQLServer/Anikets Dev/ForumDatabase/Database1/posts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[posts]
+(
+post_id         INT(8) NOT NULL AUTO_INCREMENT,
+post_content        TEXT NOT NULL,
+post_date       DATETIME NOT NULL,
+post_topic      INT(8) NOT NULL,
+post_by     INT(8) NOT NULL,
+PRIMARY KEY (post_id)
+) TYPE=INNODB;
