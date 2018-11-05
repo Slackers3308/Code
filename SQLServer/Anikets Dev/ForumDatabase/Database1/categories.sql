904240faed826b9b8a28b91969f42b6ad1e126d8
Aniket Bhatt <AniketBhatt@Anikets-MacBook-Pro-2.local>
2018-11-04 18:44:03 -0700

    REmoved useless stuff

--- SQLServer/Anikets Dev/ForumDatabase/Database1/categories.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[categories]
-(
-cat_id          INT(8) NOT NULL AUTO_INCREMENT,
-cat_name        VARCHAR(255) NOT NULL,
-cat_description     VARCHAR(255) NOT NULL,
-UNIQUE INDEX cat_name_unique (cat_name),
-PRIMARY KEY (cat_id)
-) TYPE=INNODB;
