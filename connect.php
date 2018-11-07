c98511d5a36c69885d2d10a983264d08213d9b93
Aniket Bhatt <AniketMan@users.noreply.github.com>
2018-11-06 18:45:29 -0700

    Sign in page

---
+++ connect.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+//connect.php
+$server	    = 'localhost';
+$username	= 'Any';
+$password	= '';
+$database	= 'test';
+
+/*if(!mysqli_connect($server, $username, $password))
+{
+ 	exit('Error: could not establish database connection');
+}
+if(!mysqli_select_db($database))
+{
+ 	exit('Error: could not select the database');
+}*/
+
+
+$link = mysqli_connect($server, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+/* return name of current default database */
+if ($result = mysqli_query($link, "SELECT DATABASE()")) {
+    $row = mysqli_fetch_row($result);
+    printf("Default database is %s.\n", $row[0]);
+    mysqli_free_result($result);
+}
+
+
+?>
